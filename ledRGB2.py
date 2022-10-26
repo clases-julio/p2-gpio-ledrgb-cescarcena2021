899dfa1e7a9268d18cbb8a7c9717a917a190cb46
cescarcena2021 <102520602+cescarcena2021@users.noreply.github.com>
2022-10-26 20:32:24 +0200

    Update ledRGB2.py

--- ledRGB2.py
+++ ledRGB2.py
@@ -17,13 +17,11 @@ def encender(pin):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
-    GPIO.setwarnings(False)
     
 def apagar(pin):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
-    GPIO.setwarnings(False)
     
 def encenderRojo():
     encender(rojoPin)
@@ -56,17 +54,36 @@ def encenderAmarillo():
     encender(verdePin)
     
 def encenderBlanco():
-    encender(rojoPin)
-    encender(verdePin)
-    encender(azulPin)
+   	encender(rojoPin)
+   	encender(verdePin)
+   	encender(azulPin)
+   	
+def apagarBlanco():
+	apagar(rojoPin)
+	apagar(verdePin)
+	apagar(azulPin)
+	
+def apagarMagenta():
+	apagar(rojoPin)
+	apagar(azulPin)
+	
+def apagarCyan():
+	apagar(azulPin)
+	apagar(verdePin)
+	
+def apagarAmarillo():
+	apagar(rojoPin)
+	apagar(verdePin)
+
     
 #-----Zona de declaracion de variables------#
     
 orden = input("Escoge una orden para que se ejecute en el LED...\n")
-true = True
+running = True
+GPIO.setwarnings(False)			# Desactivamos los warnings que puedan ser molestos 
 
 #----------Main-----------#
-while true:
+while running:
 
 	if orden == "encender rojo":
 		encenderRojo() 
@@ -86,23 +103,65 @@ while true:
 	elif orden == "apagar azul":
 		apagarAzul()
 		orden = input("Escoge una orden para que se ejecute en el LED...\n")
-	elif orden == "apagar todo":
-		apagarAzul()
-		apagarRojo()
-		apagarVerde()
+	elif orden == "encender blanco":
+		encenderBlanco()
 		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "apagar blanco":
+		apagarBlanco()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "apagar magenta":
+		apagarMagenta()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "apagar cyan":
+		apagarCyan()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "apagar amarillo":
+		apagarAmarillo()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "encender amarillo":
+		encenderAmarillo()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "encender cyan":
+		encenderCyan()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "encender magenta":
+		encenderMagenta()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "apagar todo":
+		GPIO.cleanup ()
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	elif orden == "encender naranja":
+		#rojo
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(rojoPin, GPIO.OUT)
+		pwm = GPIO.PWM(rojoPin, 100)
+		pwm.start(1)			## aqui se pone a 1 que realmente corresponde al 100
+		#verde
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(verdePin, GPIO.OUT)
+		pwm2 = GPIO.PWM(verdePin, 100)
+		pwm2.start(70)			## aqui es exactamente igual que con el rojo
 		
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	
+	elif orden == "apagar naranja":
+		pwm.start(0)
+		pwm2.start(0)
+		GPIO.cleanup ()
+			
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
+	
 	elif orden == "ordenes":
 		print("Las ordenes que puede reproducir este led son:\n encenedr rojo\n encender verde\n encender azul\n apagar rojo\n apagar verde\n apagar azul\n apagar todo\n salir\n")
 		orden = input("Escoge una orden para que se ejecute en el LED...\n")
 		
 	elif orden == "salir":
-		true = False
+		GPIO.cleanup ()
+		running = False
 		
 	else:
-
 		print("La orden escrita no es valida, escribe ordenes como argumento para saber las posobles ordenes")
-		break
+		orden = input("Escoge una orden para que se ejecute en el LED...\n")
 	
 
 
